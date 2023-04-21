dae543fad837e9cc3b2248cabba6a4de1b4cc854
diripar8 <diripar8@gmail.com>
2023-04-21 15:12:59 -0300

    large update

--- Redes Neuronales/Modelos_Exp_CNN.py
+++ Redes Neuronales/Modelos_Exp_CNN.py
@@ -205,12 +205,14 @@ def select_exp(model, dataset, n_exp):
         model.add(BatchNormalization())
         model.add(MaxPooling2D((2, 2)))
         model.add(Dropout(0.5))
+
         model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
         model.add(BatchNormalization())
         model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D((2, 2)))
         model.add(Dropout(0.5))
+        
         model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
         model.add(BatchNormalization())
         model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
